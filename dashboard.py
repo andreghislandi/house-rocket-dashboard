587af2624abbc0b73f619c5808facee64217866f
andre_ghislandi <deko_ghislandi@hotmail.com>
2021-04-05 14:06:56 -0300

    libs

--- dashboard.py
+++ dashboard.py
@@ -334,6 +334,127 @@ def house_rocket_map(data):
 
     return None
 
+def hypothesis (data):
+
+    st.title('Hypothesis Study')
+    display_hypothesis = st.checkbox('Display Hypothesis Study')
+
+    if display_hypothesis:
+        #H1 - waterfront properties cost 30% more (avg)
+        st.header('Hypothesis 01: Waterfront properties prices are 30% higher on average')
+        mean_waterfront = data.loc[data['waterfront']==1]['price'].mean()
+        mean_waterfront_not = data.loc[data['waterfront']==0]['price'].mean()
+        st.write(f'Waterfront properties average price: **{mean_waterfront:.2f}**')
+        st.write(f'Non-waterfront properties average price: **{mean_waterfront_not:.2f}**')
+        st.write(f'Difference in %: {(mean_waterfront/mean_waterfront_not)*100:.2f}')
+        st.markdown('Waterfront properties are actually over 300% higher on price')
+
+        #H2 - properties built before 1955 are 50% cheaper (avg)
+        st.header('Hypothesis 02: Properties built before 1955 are 50% cheaper, on average')
+        mean_under_1955 = data.loc[data['yr_built'] < 1955]['price'].mean()
+        mean_over_1955 = data.loc[data['yr_built'] >= 1955]['price'].mean()
+        st.write(f'Properties built before 1955 average price: **{mean_under_1955:.2f}**')
+        st.write(f'Properties built from 1955 on average price: **{mean_over_1955:.2f}**')
+        st.write(f'Difference in %: {100-((mean_under_1955/mean_over_1955)*100):.2f}')
+        st.markdown('Properties built before and after 1955 share a similar price average')
+
+        #H3 - properties without basement have a total lot size 50% bigger than properties with basement
+        st.header('Hypothesis 03: Properties without basement have a total lot size 50% bigger than properties with basement, on average')
+        lot_size_no_base = data.loc[data['sqft_basement'] == 0]['sqft_lot'].mean()
+        lot_size_with_base = data.loc[data['sqft_basement'] > 0]['sqft_lot'].mean()
+        st.write(f'Properties without basement average lot size: **{lot_size_no_base:.2f}**')
+        st.write(f'Properties with basement average lot size: **{lot_size_with_base:.2f}**')
+        st.write(f'Difference in %: {100-((lot_size_with_base/lot_size_no_base)*100):.2f}')
+        st.markdown('Properties without basement are on average 18% bigger on lot size than without basement.')
+
+        #H4 - properties prices YoY increase is 10%
+        st.header('Hypothesis 04: Properties prices YoY (Year over Year) increase is 10%')
+        data['date'] = pd.to_datetime( data['date'] ).dt.strftime( '%Y-%m-%d' )
+        prices_may_2014 = data.loc[(data['date'] > '2014-05-01') & (data['date'] < '2014-06-01')]['price'].mean()
+        prices_may_2015 = data.loc[(data['date'] > '2015-05-01') & (data['date'] < '2015-06-01')]['price'].mean()
+        st.write(f'Average price on May 2014: **{prices_may_2014:.2f}**')
+        st.write(f'Average price on May 2015: **{prices_may_2015:.2f}**')
+        st.write(f'Difference in %: {100-((prices_may_2014/prices_may_2015)*100):.2f}')
+        st.markdown('Year over Year increase in properties prices is only 1% (From May 2014 to May 2015)')
+
+        #H5 - properties with 3 bathrooms prices grow 15% MoM
+        st.header('Hypothesis 05: Properties with 3 bathrooms prices MoM (Month over Month) increase is 15%')
+        data['date'] = pd.to_datetime( data['date'], format='%Y-%m-%d' )
+        date_monthly = pd.date_range("2014-04-30", periods=14, freq="M")
+        percent_monthly = []
+        for i in range(len(date_monthly)-1):
+            percent_monthly.append(data.loc[(data['date'] > date_monthly[i]) & (data['date'] < date_monthly[i+1]) & (data['bathrooms']==3)]['price'].mean())
+        for i in range(len(percent_monthly)-1):
+            percent_monthly[i] = (100-((percent_monthly[i]/percent_monthly[i+1])*100))
+        percent_monthly.pop()
+
+        st.write(f'Average MoM in 12 months: **{sum(percent_monthly)/len(percent_monthly):.2f}** %')
+        
+        fig = px.line(x=date_monthly[1:13], y=percent_monthly)
+        fig.update_layout(xaxis_title='Months',
+                    yaxis_title='MoM percentage')
+        st.plotly_chart(fig, use_container_width=True)
+
+        st.markdown('Month over Month change in properties prices with 3 bathrooms is only -0.32% average on a 12 month period')
+
+        #H6 - 50% of the waterfront properties were sold during the summer(june, jul, aug)
+        st.header('Hypothesis 06: 50% of the waterfront properties were sold during the summer(june, jul, aug)')
+        df = data.loc[data['waterfront']==1][['id','date']]
+        df['date'] = pd.to_datetime( df['date'], format='%Y-%m-%d' )
+        summer_sales = df.loc[(df['date'] >= '2014-06-01') & (df['date'] < '2014-09-01')]['id'].count()
+        winter_sales = df.loc[(df['date'] > '2014-05-01') & (df['date'] < '2014-06-01') | (df['date'] >= '2014-09-01')]['id'].count()
+        st.write(f'Waterfront Houses sold on Summer: **{summer_sales}**')
+        st.write(f'Waterfront Houses sold on rest of year: **{winter_sales}**')
+        st.write(f'% to total: {(summer_sales/(summer_sales+winter_sales)*100):.2f}')
+        st.markdown('Approximately 30% of waterfront properties were sold during the summer')
+
+def houses_to_buy (data):
+
+    st.title('Suggested Houses to Buy')
+    st.markdown('- Below Median price for the region \n - Waterfront \n - Good Condition')
+    df = data.loc[(data['waterfront']==1) & (data['condition'] >=4)][['id','price','zipcode','lat','long','condition','bedrooms','bathrooms','floors','sqft_lot','yr_built']]
+    df1 = df[['price','zipcode']].groupby('zipcode').median().reset_index()
+    df1.columns = ['zipcode','zip_median']
+    df = pd.merge(df, df1, on='zipcode', how='inner')
+    df['status'] = 'N'
+    for i in range(len(df)):
+        if df.loc[i]['price'] < df.loc[i]['zip_median']:
+            df.status[i] = 'Buy'
+        if df.loc[i]['price'] >= df.loc[i]['zip_median']:
+            df.status[i] = 'No Buy'
+    st.write(df)
+
+    #draw map
+    st.header('Houses to buy: Map')
+    houses_buy = df[df['status']=='Buy']
+    fig = px.scatter_mapbox(
+            houses_buy,
+            lat='lat',
+            lon='long',
+            size='price',
+            color='condition',
+            hover_name='id',
+            hover_data=['price','condition','bedrooms','bathrooms','floors','sqft_lot','yr_built'],
+            color_continuous_scale=px.colors.sequential.Aggrnyl,
+            size_max=15,
+            zoom=9,
+    )
+
+    fig.update_layout(
+    mapbox_style="white-bg",
+    mapbox_layers=[
+        {
+            "below": 'traces',
+            "sourcetype": "raster",
+            "sourceattribution": "United States Geological Survey",
+            "source": [
+                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+            ]
+        }
+      ])
+    fig.update_layout(height=600, margin={'r':0,'t':0,'l':0,'b':0})
+    st.plotly_chart(fig)
+
 if __name__ == '__main__':
     #ETL
     #data extraction
@@ -355,3 +476,7 @@ if __name__ == '__main__':
     commercial(data)
 
     histograms(data)
+
+    hypothesis(data)
+
+    houses_to_buy(data)
